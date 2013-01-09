c04f8abf98c45b97a29828f20cb4bc32ab8d96ac
stefan <dev@2904.cc>
2013-01-09 02:31:21 +0100

    first test

---
+++ rsa/rsa_simple.py
@@ -0,0 +1,41 @@
+###########  Set up simple RSA-Environment:
+
+p = 11 # prime
+q = 13 # prime
+
+m = 42 # message
+
+###############################################################################
+
+###########  Calculate simple RSA-Environment:
+
+N = p * q
+phi = (p-1) * (q-1)
+e = 23 # Coprime of phi -> ggt(e, phi) = 1
+# TODO: "generate" by iterating down from phi and check for ggt
+
+d = 47 # e * d = 1 mod phi(N) 
+# TODO: implement extended euclidean algorith to find d (and k)
+# e * d + k * phi = 1 = ggt(e, phi)
+
+# pubkey: e, N
+# privkey: d, N
+
+print "calculated pubkey:"
+print " e = ", e, "N = ", N
+print "calculated privkey:"
+print " d = ", d, "N = ", N
+
+########### Crypt:
+
+c = pow(m,e ) % N
+
+print "encrypted message:"
+print "c = ", c
+
+###########  Decrypt: 
+
+m2 = pow(c, d) % N
+
+print "decrypted message:"
+print "m2 = ", m2
