afc3077fee18745e4cbc8d6a7885ac44e0ccfe80
stefan <dev@2904.cc>
2013-01-09 03:47:09 +0100

    extended simpliest rsa test

--- rsa/rsa_simplest.py
+++ rsa/rsa_simplest.py
@@ -15,7 +15,7 @@ e = 23 # Coprime of phi -> ggt(e, phi) = 1
 # TODO: "generate" by iterating down from phi and check for ggt
 
 d = 47 # e * d = 1 mod phi(N) 
-# TODO: implement extended euclidean algorith to find d (and k)
+# TODO: implement extended euclidean algorithm to find d (and k)
 # e * d + k * phi = 1 = ggt(e, phi)
 
 # pubkey: e, N
@@ -28,14 +28,24 @@ print " d = ", d, "N = ", N
 
 ########### Crypt:
 
-c = pow(m,e ) % N
+c = pow(m, e) % N
 
 print "encrypted message:"
-print "c = ", c
+print " c = ", c
 
 ###########  Decrypt: 
 
 m2 = pow(c, d) % N
 
 print "decrypted message:"
-print "m2 = ", m2
+print " m2 = ", m2
+
+###########  Cracking (calcluating d only knowing e and N, maybe some c): RSAP*
+
+# to calculate d, I need e and phi
+# (e is the coprime of phi!)
+# phi is Euler's totient function of N -> count the relative primes to N
+# => phi = (p-1) * (q-1) where p and q are the two (!) primes
+# because N = p*q and phi(p*q) = phi(p) * phi(q) and phi(p) = p-1
+# => Integer factorization! :(
+
